2d13f8a7506cbe42ac4d38dcd95803a5235fc552
Derwins Ochoa <111207083+derwins8a@users.noreply.github.com>
2024-05-22 22:04:53 -0400

    feat(GamesShowcase): Agg sección. Aún sin funcionalidad.

--- src/routes/GamesShowcase.tsx
+++ src/routes/GamesShowcase.tsx
@@ -2,34 +2,106 @@ import {} from "react-router-dom";
 
 export default function GamesShowcase() {
   return (
-    <div>
-      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-        <div className="md:flex">
-          <div className="md:shrink-0">
-            <img
-              className="h-48 w-full object-cover md:h-full md:w-48"
-              src="/img/building.jpg"
-              alt="Modern building architecture"
-            />
-          </div>
-          <div className="p-8">
-            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-              Company retreats
+    <section className="games">
+      <div className="flex flex-col min-h-screen container min-w-full bg-[#0F0F0F]">
+        <div className="flex flex-col items-end justify-end mr-8 mt-8 text-xl">
+          <span className="font-next_art text-white mr-11">
+            ASCENT&#40;&#41;&#123;
+          </span>
+          <span className="font-next_art text-white">DEVLOGS.&#125;&#59;</span>
+        </div>
+        <div className="cardCarousel mt-2 flex flex-row p-2">
+          <div className="card h-80 w-48 border-white border-2 mx-2 p-1 rounded-md">
+            <div className="gamePicture bg-gray-200 rounded-md h-44"></div>
+            <div className="cardsCarousel flex flex-col mt-2">
+              <span className="text-sm text-center text-white font-next_art">
+                GAME TITLE
+              </span>
+              <span className="justify-end items-end text-white font-next_art text-[8px] text-right mt-1 font-thin">
+                DÍA, 00-00-0000
+              </span>
+              <p className="text-white text-[9px] font-next_art mt-2">
+                PREVIEW TEXT
+              </p>
             </div>
-            <a
-              href="#"
-              className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
-            >
-              Incredible accommodation for your team
-            </a>
-            <p className="mt-2 text-slate-500">
-              Looking to take your team away on a retreat to enjoy awesome food
-              and take in some sunshine? We have a list of places to do just
-              that.
-            </p>
+          </div>
+          <div className="card h-80 w-48 border-white border-2 mx-2 p-1 rounded-md">
+            <div className="gamePicture bg-gray-200 rounded-md h-44"></div>
+            <div className="cardsCarousel flex flex-col mt-2">
+              <span className="text-sm text-center text-white font-next_art">
+                GAME TITLE
+              </span>
+              <span className="justify-end items-end text-white font-next_art text-[8px] text-right mt-1 font-thin">
+                DÍA, 00-00-0000
+              </span>
+              <p className="text-white text-[9px] font-next_art mt-2">
+                PREVIEW TEXT
+              </p>
+            </div>
+          </div>
+          <div className="card h-80 w-48 border-white border-2 mx-2 p-1 rounded-md">
+            <div className="gamePicture bg-gray-200 rounded-md h-44"></div>
+            <div className="cardsCarousel flex flex-col mt-2">
+              <span className="text-sm text-center text-white font-next_art">
+                GAME TITLE
+              </span>
+              <span className="justify-end items-end text-white font-next_art text-[8px] text-right mt-1 font-thin">
+                DÍA, 00-00-0000
+              </span>
+              <p className="text-white text-[9px] font-next_art mt-2">
+                PREVIEW TEXT
+              </p>
+            </div>
+          </div>
+          <div className="card h-80 w-48 border-white border-2 mx-2 p-1 rounded-md">
+            <div className="gamePicture bg-gray-200 rounded-md h-44"></div>
+            <div className="cardsCarousel flex flex-col mt-2">
+              <span className="text-sm text-center text-white font-next_art">
+                GAME TITLE
+              </span>
+              <span className="justify-end items-end text-white font-next_art text-[8px] text-right mt-1 font-thin">
+                DÍA, 00-00-0000
+              </span>
+              <p className="text-white text-[9px] font-next_art mt-2">
+                PREVIEW TEXT
+              </p>
+            </div>
+          </div>
+          <div className="card h-80 w-48 border-white border-2 mx-2 p-1 rounded-md">
+            <div className="gamePicture bg-gray-200 rounded-md h-44"></div>
+            <div className="cardsCarousel flex flex-col mt-2">
+              <span className="text-sm text-center text-white font-next_art">
+                GAME TITLE
+              </span>
+              <span className="justify-end items-end text-white font-next_art text-[8px] text-right mt-1 font-thin">
+                DÍA, 00-00-0000
+              </span>
+              <p className="text-white text-[9px] font-next_art mt-2">
+                PREVIEW TEXT
+              </p>
+            </div>
           </div>
         </div>
+        <div className="flex flex-col items-end justify-end text-xs">
+          <span className="text-white text-[8px] font-thin mr-4">&copy;ASCENT STUDIOS 2024</span>
+        </div>
+        <div className="flex-1 flex-grow"> </div>
+        <div className="flex flex-col items-center justify-center my-4">
+          <svg
+            className="h-16 w-6"
+            width="30"
+            height="147"
+            viewBox="0 0 30 147"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M17.5 3C17.5 1.61929 16.3807 0.5 15 0.5C13.6193 0.5 12.5 1.61929 12.5 3L17.5 3ZM15 146.434L29.4338 132L15 117.566L0.566249 132L15 146.434ZM12.5 3L12.5 132L17.5 132L17.5 3L12.5 3Z"
+              fill="white"
+            />
+          </svg>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
