0fc87f88f1ef56b4354a234d4cc4ec97df1ccc3f
Joso <josoalberto16@gmail.com>
2024-06-22 17:26:49 -0400

    Intergracion De Musica

--- src/components/Sticker.tsx
+++ src/components/Sticker.tsx
@@ -2,7 +2,7 @@ export const Sticker: React.FC = () => (
     <div className="absolute lg:fixed overflow-hidden z-50">
       < div className="mt-8 z-50">
 
-        <img className="h-28" src="public\images\Sticker\Sticker.png" ></img>
+        <img className="h-28" src="images\Sticker\Sticker.png" ></img>
   
       </div> 
    </div>
